213d6d8c406cb6e495c15cac0608ae92d2e55cea
afam22 <happyguy_afam@yahoo.com>
2022-10-21 23:27:08 -0600

    added the question hackerank question on staircase

--- src/com/udemylearn/hackerRank/algorithms/_8_miniMaxSum.java
+++ src/com/udemylearn/hackerRank/algorithms/_8_miniMaxSum.java
@@ -2,7 +2,22 @@ package com.udemylearn.hackerRank.algorithms;
 
 import java.util.ArrayList;
 import java.util.List;
+/*
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+ */
 public class _8_miniMaxSum {
     public static void main(String[] args) {
 
